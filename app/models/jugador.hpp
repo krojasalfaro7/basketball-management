e07630f29294d73b8dec7236e3793d6956b08bb8
Kevin Rojas <krojas.alfaro7@gmail.com>
2025-07-05 14:06:13 -0400

    Restore 2

--- app/models/jugador.hpp
+++ app/models/jugador.hpp
@@ -7,25 +7,20 @@ class Jugador
 public:
     std::string id;
     std::string nombre;
-    std::string edad;
-    std::string altura;
-    std::string peso;
-    std::string nacionalidad;
-    std::string fecha_ingreso;
     std::string posicion;
-    std::string equipo_id;
+    std::string equipo_id; // Relación con Equipo
 
     Jugador() = default;
-    Jugador(const std::string &id, const std::string &nombre, const std::string &edad, const std::string &altura, const std::string &peso, const std::string &nacionalidad, const std::string &fecha_ingreso, const std::string &posicion, const std::string &equipo_id)
-        : id(id), nombre(nombre), edad(edad), altura(altura), peso(peso), nacionalidad(nacionalidad), fecha_ingreso(fecha_ingreso), posicion(posicion), equipo_id(equipo_id) {}
+    Jugador(const std::string &id, const std::string &nombre, const std::string &posicion, const std::string &equipo_id)
+        : id(id), nombre(nombre), posicion(posicion), equipo_id(equipo_id) {}
 
     nlohmann::json to_json() const
     {
-        return {{"id", id}, {"nombre", nombre}, {"edad", edad}, {"altura", altura}, {"peso", peso}, {"nacionalidad", nacionalidad}, {"fecha_ingreso", fecha_ingreso}, {"posicion", posicion}, {"equipo_id", equipo_id}};
+        return {{"id", id}, {"nombre", nombre}, {"posicion", posicion}, {"equipo_id", equipo_id}};
     }
 
     static Jugador from_json(const nlohmann::json &j)
     {
-        return Jugador(j.at("id"), j.at("nombre"), j.at("edad"), j.at("altura"), j.at("peso"), j.at("nacionalidad"), j.at("fecha_ingreso"), j.at("posicion"), j.at("equipo_id"));
+        return Jugador(j.at("id"), j.at("nombre"), j.at("posicion"), j.at("equipo_id"));
     }
 };
